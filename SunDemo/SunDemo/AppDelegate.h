0ea7dcb697ba8776d16f7863d842395596ddc4d8
SunOnline <767272127@qq.com>
2016-03-24 14:46:53 +0800

    新建工程

---
+++ SunDemo/SunDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SunDemo
+//
+//  Created by students on 16/3/24.
+//  Copyright © 2016年 students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SunDemo/SunDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SunDemo
+//
+//  Created by students on 16/3/24.
+//  Copyright © 2016年 students. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
